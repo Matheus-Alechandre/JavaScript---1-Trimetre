9cf32292c64bcd6a63dae998e66e04f360fdf81a
Matheus-Alechandre <162439808+Matheus-Alechandre@users.noreply.github.com>
2024-05-15 11:49:11 -0300

    Add files via upload

---
+++ for.js
@@ -0,0 +1,6 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+                                //indice++ ou indice +=1 ou indice == indice + 1
+for(let indice = 0; indice < numeros.length; indice += 1){
+    console.log(numeros[indice])
+};
